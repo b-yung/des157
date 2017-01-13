0420862f2a60ece8031bb02b38f7871031eb44ce
b-yung <byung13@scc2102-15.ucdavis.edu>
2017-01-12 16:29:31 -0800

    Add blog and studio
    portal and blog

---
+++ script.js
@@ -0,0 +1,21 @@
+// p5 file
+
+// open the console window of any browser to see console.log() messages,
+// like print() messages in Processing
+console.log("this is a js comment");
+
+function setup() {
+  var myCanvas=createCanvas(800, 250);
+  myCanvas.parent('mySketch');
+  background(39,55,70);
+}
+
+function draw() {
+  // statements here
+  if(mouseIsPressed){
+  fill (0);
+} else {
+  fill (255);
+}
+  ellipse(mouseX, mouseY, 50, 50);
+}
