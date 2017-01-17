ad3a1cadbb42ebe5b7d814d07623890935550697
b-yung <byung13@scc2102-15.ucdavis.edu>
2017-01-17 15:46:52 -0800

    finish js

--- script.js
+++ script.js
@@ -3,68 +3,73 @@ console.log("this is a js comment");
 function setup() {
   var myCanvas=createCanvas(800, 250);
   myCanvas.parent('mySketch');
-  background(39,55,70);
 }
 
 function draw() {
   noStroke(); // turn off for different experience
   fill(122,231,199); //MINT
-  rect(0,0,799,799); //FIRST RECTANGLE
+  rect(0,0,800,250); //FIRST RECTANGLE
   rect(50,50,700,150); //SECOND RECTANGLE
   rect(100,100,600,50); //THIRD RECTANGLE
-  rect(240,240,320,320); //FOURTH RECTANGLE
-  rect(320,320,160,160); //FIFTH RECTANGLE
 
-//FIRST RECTANGLE
+//RECTANGLE ZERO
 if ((mouseX>0)&&(mouseX<50)) {
     fill(108,128,154); //SLATE BLUE
-    rect(0,0,799,799); // COLORED RECTANGLE
+    rect(0,0,800,250); // COLORED RECTANGLE
     fill(122,231,199); // MINT
     rect(50,50,700,150); //SECOND RECTANGLE
     rect(100,100,600,50); //THIRD RECTANGLE
   }
-if ((mouseX>750)&&(mouseX<500)) {
+if ((mouseX>750)&&(mouseX<800)) {
     fill(108,128,154); //SLATE BLUE
-    rect(0,0,799,799); // COLORED RECTANGLE
+    rect(0,0,800,250); // COLORED RECTANGLE
     fill(122,231,199); // MINT
     rect(50,50,700,150); //SECOND RECTANGLE
     rect(100,100,600,50); //THIRD RECTANGLE
   }
 if ((mouseY>0)&&(mouseY<50)) {
     fill(108,128,154); //SLATE BLUE
-    rect(0,0,799,799);  // COLORED RECTANGLE
+    rect(0,0,800,250);  // COLORED RECTANGLE
     fill(122,231,199); // MINT
     rect(50,50,700,150); //SECOND RECTANGLE
     rect(100,100,600,50); //THIRD RECTANGLE
 }
 if ((mouseY>200)&&(mouseY<250)) {
     fill(108,128,154); //SLATE BLUE
-    rect(0,0,799,799);  // COLORED RECTANGLE
+    rect(0,0,800,250);  // COLORED RECTANGLE
     fill(122,231,199); // MINT
     rect(50,50,700,150); //SECOND RECTANGLE
     rect(100,100,600,50); //THIRD RECTANGLE
 }
 
-// SECOND RECTANGLE
+// RECTANGLE ONE
 if ((mouseX>50)&&(mouseX<100)) {
+    fill(122,231,199); // MINT
+    rect(0,0,800,250); //FIRST RECTANGLE
     fill(108,128,154); //SLATE BLUE
-    rect(50,50,700,150); //SECOND RECTANGLE // COLORED RECTANGLE
+    rect(50,50,700,150); //SECOND RECTANGLE
     fill(122,231,199); // MINT
     rect(100,100,600,50); //THIRD RECTANGLE
   }
-if ((mouseX>200)&&(mouseX<250)) {
+if ((mouseX>700)&&(mouseX<750)) {
+    fill(122,231,199); // MINT
+    rect(0,0,800,250); //FIRST RECTANGLE
     fill(108,128,154); //SLATE BLUE
     rect(50,50,700,150); //SECOND RECTANGLE // COLORED RECTANGLE
     fill(122,231,199); // MINT
     rect(100,100,600,50); //THIRD RECTANGLE
 }
 if ((mouseY>50)&&(mouseY<100)) {
+  fill(122,231,199); // MINT
+  rect(0,0,800,250); //FIRST RECTANGLE
     fill(108,128,154); //SLATE BLUE
     rect(50,50,700,150); //SECOND RECTANGLE // COLORED RECTANGLE
     fill(122,231,199); // MINT
     rect(100,100,600,50); //THIRD RECTANGLE
 }
-if ((mouseY>200)&&(mouseY<250)) {
+if ((mouseY>150)&&(mouseY<200)) {
+  fill(122,231,199); // MINT
+  rect(0,0,800,250); //FIRST RECTANGLE
     fill(108,128,154); //SLATE BLUE
     rect(50,50,700,150); //SECOND RECTANGLE // COLORED RECTANGLE
     fill(122,231,199); // MINT
@@ -72,25 +77,11 @@ if ((mouseY>200)&&(mouseY<250)) {
 }
 
 // THIRD RECTANGLE
-if ((mouseX>100)&&(mouseX<150)) {
+if ((mouseX>100)&&(mouseX<700)&&(mouseY>100)&&(mouseY<150)) {
+    fill(122,231,199); // MINT
+    rect(0,0,800,250); //RECTANGLE ZERO
     fill(108,128,154); //SLATE BLUE
     rect(100,100,600,50); //THIRD RECTANGLE // COLORED RECTANGLE
-    fill(122,231,199); // MINT
-  }
-  if ((mouseX>100)&&(mouseX<150)) {
-    fill(108,128,154); //SLATE BLUE
-    rect(100,100,600,50); //THIRD RECTANGLE // COLORED RECTANGLE
-    fill(122,231,199); // MINT
-}
-  if ((mouseY>100)&&(mouseY<150)) {
-    fill(108,128,154); //SLATE BLUE
-    rect(100,100,600,50); //THIRD RECTANGLE // COLORED RECTANGLE
-    fill(122,231,199); // MINT
-}
-  if ((mouseY>100)&&(mouseY<150)) {
-    fill(108,128,154); //SLATE BLUE
-    rect(100,100,600,50); //THIRD RECTANGLE // COLORED RECTANGLE
-    fill(122,231,199); // MINT
   }
 
 /*
