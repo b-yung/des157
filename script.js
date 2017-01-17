648fd4477d3399ffdaa7049253a32ef003c83319
b-yung <byung13@scc2102-15.ucdavis.edu>
2017-01-17 15:20:43 -0800

    update script.js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// p5 file
-
-// open the console window of any browser to see console.log() messages,
-// like print() messages in Processing
 console.log("this is a js comment");
 
 function setup() {
@@ -11,11 +7,122 @@ function setup() {
 }
 
 function draw() {
-  // statements here
-  if(mouseIsPressed){
-  fill (0);
-} else {
-  fill (255);
+  noStroke(); // turn off for different experience
+  fill(122,231,199); //MINT
+  rect(0,0,799,799); //FIRST RECTANGLE
+  rect(50,50,700,150); //SECOND RECTANGLE
+  rect(100,100,600,50); //THIRD RECTANGLE
+  rect(240,240,320,320); //FOURTH RECTANGLE
+  rect(320,320,160,160); //FIFTH RECTANGLE
+
+//FIRST RECTANGLE
+if ((mouseX>0)&&(mouseX<50)) {
+    fill(108,128,154); //SLATE BLUE
+    rect(0,0,799,799); // COLORED RECTANGLE
+    fill(122,231,199); // MINT
+    rect(50,50,700,150); //SECOND RECTANGLE
+    rect(100,100,600,50); //THIRD RECTANGLE
+  }
+if ((mouseX>750)&&(mouseX<500)) {
+    fill(108,128,154); //SLATE BLUE
+    rect(0,0,799,799); // COLORED RECTANGLE
+    fill(122,231,199); // MINT
+    rect(50,50,700,150); //SECOND RECTANGLE
+    rect(100,100,600,50); //THIRD RECTANGLE
+  }
+if ((mouseY>0)&&(mouseY<50)) {
+    fill(108,128,154); //SLATE BLUE
+    rect(0,0,799,799);  // COLORED RECTANGLE
+    fill(122,231,199); // MINT
+    rect(50,50,700,150); //SECOND RECTANGLE
+    rect(100,100,600,50); //THIRD RECTANGLE
 }
-  ellipse(mouseX, mouseY, 50, 50);
+if ((mouseY>200)&&(mouseY<250)) {
+    fill(108,128,154); //SLATE BLUE
+    rect(0,0,799,799);  // COLORED RECTANGLE
+    fill(122,231,199); // MINT
+    rect(50,50,700,150); //SECOND RECTANGLE
+    rect(100,100,600,50); //THIRD RECTANGLE
+}
+
+// SECOND RECTANGLE
+if ((mouseX>50)&&(mouseX<100)) {
+    fill(108,128,154); //SLATE BLUE
+    rect(50,50,700,150); //SECOND RECTANGLE // COLORED RECTANGLE
+    fill(122,231,199); // MINT
+    rect(100,100,600,50); //THIRD RECTANGLE
+  }
+if ((mouseX>200)&&(mouseX<250)) {
+    fill(108,128,154); //SLATE BLUE
+    rect(50,50,700,150); //SECOND RECTANGLE // COLORED RECTANGLE
+    fill(122,231,199); // MINT
+    rect(100,100,600,50); //THIRD RECTANGLE
+}
+if ((mouseY>50)&&(mouseY<100)) {
+    fill(108,128,154); //SLATE BLUE
+    rect(50,50,700,150); //SECOND RECTANGLE // COLORED RECTANGLE
+    fill(122,231,199); // MINT
+    rect(100,100,600,50); //THIRD RECTANGLE
+}
+if ((mouseY>200)&&(mouseY<250)) {
+    fill(108,128,154); //SLATE BLUE
+    rect(50,50,700,150); //SECOND RECTANGLE // COLORED RECTANGLE
+    fill(122,231,199); // MINT
+    rect(100,100,600,50); //THIRD RECTANGLE
+}
+
+// THIRD RECTANGLE
+if ((mouseX>100)&&(mouseX<150)) {
+    fill(108,128,154); //SLATE BLUE
+    rect(100,100,600,50); //THIRD RECTANGLE // COLORED RECTANGLE
+    fill(122,231,199); // MINT
+  }
+  if ((mouseX>100)&&(mouseX<150)) {
+    fill(108,128,154); //SLATE BLUE
+    rect(100,100,600,50); //THIRD RECTANGLE // COLORED RECTANGLE
+    fill(122,231,199); // MINT
+}
+  if ((mouseY>100)&&(mouseY<150)) {
+    fill(108,128,154); //SLATE BLUE
+    rect(100,100,600,50); //THIRD RECTANGLE // COLORED RECTANGLE
+    fill(122,231,199); // MINT
+}
+  if ((mouseY>100)&&(mouseY<150)) {
+    fill(108,128,154); //SLATE BLUE
+    rect(100,100,600,50); //THIRD RECTANGLE // COLORED RECTANGLE
+    fill(122,231,199); // MINT
+  }
+
+/*
+// FOURTH RECTANGLE
+if ((mouseX>240)&&(mouseX<320)) {
+    fill(249,165,137);
+    rect(240,240,320,320); // COLORED RECTANGLE
+    fill(122,231,199);
+    rect(320,320,160,160);
+  }
+  if ((mouseX>480)&&(mouseX<560)) {
+    fill(249,165,137);
+    rect(240,240,320,320); // COLORED RECTANGLE
+    fill(122,231,199);
+    rect(320,320,160,160);  }
+  if ((mouseY>240)&&(mouseY<320)) {
+    fill(249,165,137);
+    rect(240,240,320,320); // COLORED RECTANGLE
+    fill(122,231,199);
+    rect(320,320,160,160);  }
+  if ((mouseY>480)&&(mouseY<560)) {
+    fill(249,165,137);
+    rect(240,240,320,320); // COLORED RECTANGLE
+    fill(122,231,199);
+    rect(320,320,160,160);
+  }
+
+
+// SMALLEST RECTANGLE
+if ((mouseX>320)&&(mouseX<480)&&(mouseY>320)&&(mouseY<480)) {
+    fill(108,128,154); //SLATE BLUE
+    rect(320,320,160,160); // COLORED RECTANGLE
+  }
+*/
 }
--- script2.js
+++ script2.js
@@ -1,114 +1,96 @@
 console.log("this is a js comment");
 
 function setup() {
-  var myCanvas=createCanvas(800, 800);
+  var myCanvas=createCanvas(800, 250);
   myCanvas.parent('mySketch');
   background(39,55,70);
 }
 
 function draw() {
   noStroke(); // turn off for different experience
-  fill(249,118,128); //PINK
-  rect(0,0,799,799);
-  rect(80,80,640,640);
-  rect(160,160,480,480);
-  rect(240,240,320,320);
-  rect(320,320,160,160);
+  fill(122,231,199); //MINT
+  rect(0,0,799,799); //FIRST RECTANGLE
+  rect(50,50,700,150); //SECOND RECTANGLE
+  rect(100,100,600,50); //THIRD RECTANGLE
+  rect(240,240,320,320); //FOURTH RECTANGLE
+  rect(320,320,160,160); //FIFTH RECTANGLE
 
 //BIG RECTANGLE
-if ((mouseX>0)&&(mouseX<80)) {
-    fill(249,165,137); //ORANGE
+if ((mouseX>0)&&(mouseX<50)) {
+    fill(108,128,154); //SLATE BLUE
     rect(0,0,799,799); // COLORED RECTANGLE
-    fill(249,118,128); //PINK
-    rect(80,80,640,640);
-    rect(160,160,480,480);
-    rect(240,240,320,320);
-    rect(320,320,160,160);
+    fill(122,231,199); // MINT
+    rect(50,50,700,150); //SECOND RECTANGLE
+    rect(100,100,600,50); //THIRD RECTANGLE
   }
-  if ((mouseX>720)&&(mouseX<800)) {
-    fill(249,165,137); //ORANGE
+  if ((mouseX>750)&&(mouseX<500)) {
+    fill(108,128,154); //SLATE BLUE
     rect(0,0,799,799); // COLORED RECTANGLE
-    fill(249,118,128); //PINK
-    rect(80,80,640,640);
-    rect(160,160,480,480);
-    rect(240,240,320,320);
-    rect(320,320,160,160);  }
+    fill(122,231,199); // MINT
+    rect(50,50,700,150); //SECOND RECTANGLE
+    rect(100,100,600,50); //THIRD RECTANGLE
+  }
   if ((mouseY>0)&&(mouseY<80)) {
-    fill(249,165,137); //ORANGE
+    fill(108,128,154); //SLATE BLUE
     rect(0,0,799,799);  // COLORED RECTANGLE
-    fill(249,118,128);
-    rect(80,80,640,640);
-    rect(160,160,480,480);
-    rect(240,240,320,320);
-    rect(320,320,160,160);  }
-  if ((mouseY>720)&&(mouseY<800)) {
-    fill(249,165,137); //ORANGE
+    fill(122,231,199); // MINT
+    rect(50,50,700,150); //SECOND RECTANGLE
+    rect(100,100,600,50); //THIRD RECTANGLE
+}
+  if ((mouseY>750)&&(mouseY<800)) {
+    fill(108,128,154); //SLATE BLUE
     rect(0,0,799,799);  // COLORED RECTANGLE
-    fill(249,118,128);
-    rect(80,80,640,640);
-    rect(160,160,480,480);
-    rect(240,240,320,320);
-    rect(320,320,160,160);
-  }
+    fill(122,231,199); // MINT
+    rect(50,50,700,150); //SECOND RECTANGLE
+    rect(100,100,600,50); //THIRD RECTANGLE
+}
 
 // SECOND RECTANGLE
 if ((mouseX>80)&&(mouseX<160)) {
-    fill(249,165,137); //ORANGE
-    rect(80,80,640,640); // COLORED RECTANGLE
-    fill(249,118,128);
-    rect(160,160,480,480);
-    rect(240,240,320,320);
-    rect(320,320,160,160);
+    fill(108,128,154); //SLATE BLUE
+    rect(50,50,700,150); //SECOND RECTANGLE // COLORED RECTANGLE
+    fill(122,231,199); // MINT
+    rect(100,100,600,50); //THIRD RECTANGLE
   }
-  if ((mouseX>640)&&(mouseX<720)) {
-    fill(249,165,137); //ORANGE
-    rect(80,80,640,640); // COLORED RECTANGLE
-    fill(249,118,128);
-    rect(160,160,480,480);
-    rect(240,240,320,320);
-    rect(320,320,160,160);  }
+  if ((mouseX>640)&&(mouseX<750)) {
+    fill(108,128,154); //SLATE BLUE
+    rect(50,50,700,150); //SECOND RECTANGLE // COLORED RECTANGLE
+    fill(122,231,199); // MINT
+    rect(100,100,600,50); //THIRD RECTANGLE
+}
   if ((mouseY>80)&&(mouseY<160)) {
-    fill(249,165,137); //ORANGE
-    rect(80,80,640,640); // COLORED RECTANGLE
-    fill(249,118,128); //PINK
-    rect(160,160,480,480);
-    rect(240,240,320,320);
-    rect(320,320,160,160);  }
-  if ((mouseY>640)&&(mouseY<720)) {
-    fill(249,165,137); //ORANGE
-    rect(80,80,640,640); // COLORED RECTANGLE
-    fill(249,118,128); //PINK
-    rect(160,160,480,480);
-    rect(240,240,320,320);
-    rect(320,320,160,160);
-  }
+    fill(108,128,154); //SLATE BLUE
+    rect(50,50,700,150); //SECOND RECTANGLE // COLORED RECTANGLE
+    fill(122,231,199); // MINT
+    rect(100,100,600,50); //THIRD RECTANGLE
+}
+  if ((mouseY>640)&&(mouseY<750)) {
+    fill(108,128,154); //SLATE BLUE
+    rect(50,50,700,150); //SECOND RECTANGLE // COLORED RECTANGLE
+    fill(122,231,199); // MINT
+    rect(100,100,600,50); //THIRD RECTANGLE
+}
 
 // THIRD RECTANGLE
 if ((mouseX>160)&&(mouseX<240)) {
-    fill(249,165,137); //ORANGE
-    rect(160,160,480,480); // COLORED RECTANGLE
-    fill(249,118,128); //PINK
-    rect(240,240,320,320);
-    rect(320,320,160,160);
+    fill(108,128,154); //SLATE BLUE
+    rect(100,100,600,50); //THIRD RECTANGLE // COLORED RECTANGLE
+    fill(122,231,199); // MINT
   }
   if ((mouseX>560)&&(mouseX<640)) {
-    fill(249,165,137); //ORANGE
-    rect(160,160,480,480); // COLORED RECTANGLE
-    fill(249,118,128); //PINK
-    rect(240,240,320,320);
-    rect(320,320,160,160);  }
+    fill(108,128,154); //SLATE BLUE
+    rect(100,100,600,50); //THIRD RECTANGLE // COLORED RECTANGLE
+    fill(122,231,199); // MINT
+}
   if ((mouseY>160)&&(mouseY<240)) {
-    fill(249,165,137); //ORANGE
-    rect(160,160,480,480); // COLORED RECTANGLE
-    fill(249,118,128); //PINK
-    rect(240,240,320,320);
-    rect(320,320,160,160);  }
+    fill(108,128,154); //SLATE BLUE
+    rect(100,100,600,50); //THIRD RECTANGLE // COLORED RECTANGLE
+    fill(122,231,199); // MINT
+}
   if ((mouseY>560)&&(mouseY<640)) {
-    fill(249,165,137); //ORANGE
-    rect(160,160,480,480); // COLORED RECTANGLE
-    fill(249,118,128); //PINK
-    rect(240,240,320,320);
-    rect(320,320,160,160);
+    fill(108,128,154); //SLATE BLUE
+    rect(100,100,600,50); //THIRD RECTANGLE // COLORED RECTANGLE
+    fill(122,231,199); // MINT
   }
 
 
@@ -116,30 +98,30 @@ if ((mouseX>160)&&(mouseX<240)) {
 if ((mouseX>240)&&(mouseX<320)) {
     fill(249,165,137);
     rect(240,240,320,320); // COLORED RECTANGLE
-    fill(249,118,128);
+    fill(122,231,199);
     rect(320,320,160,160);
   }
   if ((mouseX>480)&&(mouseX<560)) {
     fill(249,165,137);
     rect(240,240,320,320); // COLORED RECTANGLE
-    fill(249,118,128);
+    fill(122,231,199);
     rect(320,320,160,160);  }
   if ((mouseY>240)&&(mouseY<320)) {
     fill(249,165,137);
     rect(240,240,320,320); // COLORED RECTANGLE
-    fill(249,118,128);
+    fill(122,231,199);
     rect(320,320,160,160);  }
   if ((mouseY>480)&&(mouseY<560)) {
     fill(249,165,137);
     rect(240,240,320,320); // COLORED RECTANGLE
-    fill(249,118,128);
+    fill(122,231,199);
     rect(320,320,160,160);
   }
 
 
 // SMALLEST RECTANGLE
 if ((mouseX>320)&&(mouseX<480)&&(mouseY>320)&&(mouseY<480)) {
-    fill(249,165,137); //orange
+    fill(108,128,154); //SLATE BLUE
     rect(320,320,160,160); // COLORED RECTANGLE
   }
 
