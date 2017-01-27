f2a46449e6af9be996b08dfd35e64011be62805b
b-yung <byung13@scc2102-21.ucdavis.edu>
2017-01-26 16:27:02 -0800

    bananas

--- studio 2/script.js
+++ studio 2/script.js
@@ -26,6 +26,29 @@ function processForm() {
 
     //call a new alert() to concatenate a message with userName and userColor
     myMsg.innerHTML = 'Hi ' + userName + ', my favorite color is ' + userColor + ', too';
+    myMsg.innerHTML = 'What Can I Say About this 571B ' + noun1 Slicer + that hasn't already been said about the wheel, penicillin, or the iPhone... this is one of the greatest inventions of all time.
+
+    My husband and I would ague constantly over who had to cut the day's noun1 slices.
+
+    It's one of those chores NO ONE wants to do! You know, the old "I spent the entire day rearing OUR children, maybe YOU can pitch in a little and cut these noun1s?"
+
+    and of course, "You think I have the energy to slave over your damn noun1s? I worked a 12 hour shift just to come home to THIS?!"
+
+    These are the things that can destroy an entire relationship. It got to the point where our children could sense the tension.
+
+    The minute I heard our 6-year-old girl in her bedroom, re-enacting our daily noun1 fight with her Barbie dolls, I knew we had to make a change.
+
+    That's when I found the 571B noun1 Slicer.
+
+    Our marriage has never been healthier,
+
+    AND we've even incorporated it into our lovemaking.
+
+    THANKS 571B noun1 SLICER! '
+
+
+
+
 
     my Msg.className = "show";
 
