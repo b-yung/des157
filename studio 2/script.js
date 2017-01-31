309bcf63f96f5d82d05cb75436300718d2a35bad
b-yung <bcyung@ucdavis.edu>
2017-01-31 02:04:10 -0800

    push marlins

--- studio 2/script.js
+++ studio 2/script.js
@@ -1,63 +1,66 @@
 // JavaScript Document
+console.log('reading');
 
 document.addEventListener("DOMContentLoaded", function(event) {
     console.log("DOM fully loaded and parsed");
 
-//capture the submit event
-document.f.onsubmit = processForm;
-document.f.onsubmit = clearForm;
 
-//define process function
-function processForm() {
+    //capture the submit event
+    document.f.onsubmit = processForm;
+    document.f.onreset = clearForm;
 
-    //store user name in a variable
-    var userName = document.f.userName.value;
-    // alert('Hi ' + userName);
+    //define process function
+    function processForm() {
 
-    //store userColor in a variable called userColor
-    var userColor = document.f.userColor.value;
-    //comment out the alert message above
+        //store names in variables
+        var userName = document.f.userName.value;
 
-    var myMsg = document.getElementById('myMsg');
+        var num1 = document.f.num1.value;
 
-    //if fields are not complete
+        var letter = document.f.letter.value;
 
-    //if fields are complete
+        var noun1 = document.f.noun1.value;
 
-    //call a new alert() to concatenate a message with userName and userColor
-    myMsg.innerHTML = 'Hi ' + userName + ', my favorite color is ' + userColor + ', too';
-    myMsg.innerHTML = 'What Can I Say About this 571B ' + noun1 Slicer + that hasn't already been said about the wheel, penicillin, or the iPhone... this is one of the greatest inventions of all time.
+        var prod1 = document.f.prod1.value;
 
-    My husband and I would ague constantly over who had to cut the day's noun1 slices.
+        var adj1 = document.f.adj1.value;
 
-    It's one of those chores NO ONE wants to do! You know, the old "I spent the entire day rearing OUR children, maybe YOU can pitch in a little and cut these noun1s?"
+        var action = document.f.action.value;
 
-    and of course, "You think I have the energy to slave over your damn noun1s? I worked a 12 hour shift just to come home to THIS?!"
+        var num2 = document.f.num2.value;
 
-    These are the things that can destroy an entire relationship. It got to the point where our children could sense the tension.
+        var org = document.f.org.value;
 
-    The minute I heard our 6-year-old girl in her bedroom, re-enacting our daily noun1 fight with her Barbie dolls, I knew we had to make a change.
+        var dollname = document.f.dollname.value;
 
-    That's when I found the 571B noun1 Slicer.
+        var noun2 = document.f.noun2.value;
 
-    Our marriage has never been healthier,
-
-    AND we've even incorporated it into our lovemaking.
-
-    THANKS 571B noun1 SLICER! '
+        var myMsg = document.getElementById('myMsg');
 
 
+        myMsg.innerHTML = 'By ' + userName + "<br />" + 'What Can I Say About this ' + num1 + letter + ' ' + noun1 + 'Slicer that has not already been said about the wheel, penicillin, or the i' + prod1 + '... this is one of the greatest inventions of all time. My husband and I would argue constantly over who had to cut the ' +  noun1 + 'slices. It is one of those chores NO ONE wants to do! You know, the old "I spent the entire day ' +  action + 'ing OUR children, maybe YOU can pitch in a little and cut these' + noun1 + 's?" and of course, "You think I have the energy to slave over your ' + adj1 + ' ' + noun1 + 's? I worked a ' + num2 + 'hour shift just to come home to THIS?!" These are the things that can destroy an entire relationship. It got to the point where our children could sense the tension. The minute I heard our 6-year-old ' + org + ' in her bedroom, re-enacting our daily ' + noun1 +
+        ' fight with her '+ dollname + ' dolls, I knew we had to make a change. That is when I found the ' + num1 + letter + ' ' + noun1 + 'Slicer. Our marriage has never been healthier, AND we have even incorporated it into our ' + noun2 + 'making.'
+        'THANKS ' + num1 + letter + ' ' + noun1 + 'SLICER!'
 
+        ;
+        myMsg.className = "show";
+    /*
+        if (userName = "" || userColor = "") {
+            alert('please fill out!');
+        } else {
+            myMsg.className = "show";
+            myMsg.innerHTML = 'Hi ' + userName + ' I also like ' + userColor;
 
+        }
+  */
+        //prevent page from reloading
+        return false;
 
-    my Msg.className = "show";
+    }
 
-    //prevent page from reloading
-    return false;
-
+    //clear form
     function clearForm() {
         myMsg.className = "hide";
     }
 
-}
 });
