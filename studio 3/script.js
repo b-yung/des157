d88158f3042a2dcc5bbf68dc554912e24f7fefed
b-yung <bcyung@ucdavis.edu>
2017-02-05 22:36:20 -0800

    vandal punch bad console

--- studio 3/script.js
+++ studio 3/script.js
@@ -6,25 +6,31 @@ document.addEventListener("DOMContentLoaded", function(event) {
     // add event listeners
     //var original = document.getElementById('original');
     var tag = document.getElementById('tag');
-/*    var tag_txt = document.getElementById('tag_txt');
-
     var vandal = document.getElementById('vandal');
     var punch = document.getElementById('punch');
     var bad = document.getElementById('bad');
     var marvel = document.getElementById('marvel');
     var gb = document.getElementById('gb');
 */
-        });
+
 
     // ***************************************************************
     // change tag color
 
     // add an eventListener for mouseover that changes the src of the
     // tag image to the image titled "tag.png"
-    tag.addEventListener('mouseover', function() {
+    /*tag.addEventListener('mouseover', function() {
         console.log('mouseover, change color');
         tag.src = "img/bw/tag.png"
+    });*/
+
+    vandal.addEventListener('mouseover', function() {
+        console.log('mouseover, change color');
+        vandal.src = "img/bw/vandal.png"
     });
+
+
+          });
 /*
     tag.addEventListener('mouseout', function() {
         console.log('mouseout, tag b&w');
--- studio3/script.js
+++ studio3/script.js
@@ -6,25 +6,61 @@ document.addEventListener("DOMContentLoaded", function(event) {
     // add event listeners
     //var original = document.getElementById('original');
     var tag = document.getElementById('tag');
-/*    var tag_txt = document.getElementById('tag_txt');
-
+    console.log(tag);
     var vandal = document.getElementById('vandal');
     var punch = document.getElementById('punch');
     var bad = document.getElementById('bad');
     var marvel = document.getElementById('marvel');
     var gb = document.getElementById('gb');
-*/
-        });
+
+
 
     // ***************************************************************
     // change tag color
 
     // add an eventListener for mouseover that changes the src of the
     // tag image to the image titled "tag.png"
-    tag.addEventListener('mouseover', function() {
+    /*tag.addEventListener('mouseover', function() {
         console.log('mouseover, change color');
         tag.src = "img/bw/tag.png"
+    });*/
+
+
+    vandal.addEventListener('mouseover', function() {
+        console.log('mouseover, change color');
+        vandal.src = "img/bw/vandal.png"
     });
+
+    vandal.addEventListener('mouseout', function() {
+        console.log('mouseout, vandal b&w');
+        vandal.src = "img/bw/vandal-bw.png";
+    });
+
+    // ***************************************************************
+    // change punch color
+    punch.addEventListener('mouseover', function() {
+        console.log('mouseover, change color');
+        punch.src = "img/bw/punch.png"
+    });
+
+    punch.addEventListener('mouseout', function() {
+        console.log('mouseout, punch b&w');
+        punch.src = "img/bw/punch-bw.png";
+    });
+
+    // ***************************************************************
+    // change bad color
+    bad.addEventListener('mouseover', function() {
+        console.log('mouseover, change color');
+        bad.src = "img/bw/bad.png"
+    });
+
+    bad.addEventListener('mouseout', function() {
+        console.log('mouseout, bad b&w');
+        bad.src = "img/bw/bad-bw.png";
+    });
+
+});
 /*
     tag.addEventListener('mouseout', function() {
         console.log('mouseout, tag b&w');
