6617640d613767c39c9a445d3573ccb306dbc462
b-yung <bcyung@ucdavis.edu>
2017-02-06 01:18:27 -0800

    no idea

--- studio3/script.js
+++ studio3/script.js
@@ -3,25 +3,26 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     console.log("DOM fully loaded and parsed");
 
-    var tag = document.getElementById('tag');
-    var vandal = document.getElementById('vandal');
-    var punch = document.getElementById('punch');
-    var bad = document.getElementById('bad');
-    var marvels = document.getElementById('marvels');
-    var gb = document.getElementById('gb');
+    var tag = document.getElementById("tag");
+    var vandal = document.getElementById("vandal");
+    var punch = document.getElementById("punch");
+    var bad = document.getElementById("bad");
+    var marvels = document.getElementById("marvels");
+    var gb = document.getElementById("gb");
 
 
     var tagAnswer = document.getElementById("tagAnswer");
+  /*
     var vandalAnswer = document.getElementById("vandalAnswer");
     var punchAnswer = document.getElementById("punchAnswer");
     var badAnswer = document.getElementById("badAnswer");
     var marvelAnswer = document.getElementById("marvelAnswer");
     var gbAnswer = document.getElementById("gbAnswer");
-
-
+    */
+/*
     var answer = document.getElementsByClassName("answer");
     var close = document.getElementsByClassName("close");
-
+*/
     /*
     var answers = [tagAnswer, vandalAnswer, punchAnswer, badAnswer, marvelAnswer, gbAnswer];
     var imgs = [tag, vandal, punch, bad, marvels, gb];
@@ -30,110 +31,111 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 // -----------------------------------------------------
     // change tag color
-    tag.addEventListener('mouseover', function() {
-        console.log('mouseover, change color');
-        tag.src = "img/bw/tag.png"
+    tag.addEventListener("mouseover", function() {
+        console.log("mouseover, change color");
+        tag.src = "img/bw/tag.png";
     });
 
-    tag.addEventListener('mouseout', function() {
-        console.log('mouseout, tag b&w');
+    tag.addEventListener("mouseout", function() {
+        console.log("mouseout, tag b&w");
         tag.src = "img/bw/tag-bw.png";
     });
 
-    tag.addEventListener('click', function() {
-        console.log('tag clicked');
+    tag.addEventListener("click", function() {
+        console.log("tag clicked");
         tagAnswer.style.display="block";
         /*document.getElementById("tagAnswer").className = "answer"; */
-    })
+    });
+
 
 // -----------------------------------------------------
     // change vandal color
-    vandal.addEventListener('mouseover', function() {
-        console.log('mouseover, change color');
-        vandal.src = "img/bw/vandal.png"
+    vandal.addEventListener("mouseover", function() {
+        console.log("mouseover, change color");
+        vandal.src = "img/bw/vandal.png";
     });
 
-    vandal.addEventListener('mouseout', function() {
-        console.log('mouseout, vandal b&w');
+    vandal.addEventListener("mouseout", function() {
+        console.log("mouseout, vandal b&w");
         vandal.src = "img/bw/vandal-bw.png";
     });
 
-    vandal.addEventListener('click', function() {
-        console.log('vandal clicked');
-        vandalAnswer.style.display = 'block';
-        document.getElementById("vandalAnswer").className = "answer";
-    })
+    vandal.addEventListener("click", function() {
+        console.log("vandal clicked");
+        vandalAnswer.style.display = "block";
+        //document.getElementById("vandalAnswer").className = "answer";
+    });
 
 // -----------------------------------------------------
     // change punch color
-    punch.addEventListener('mouseover', function() {
-        console.log('mouseover, change color');
-        punch.src = "img/bw/punch.png"
+    punch.addEventListener("mouseover", function() {
+        console.log("mouseover, change color");
+        punch.src = "img/bw/punch.png";
     });
 
-    punch.addEventListener('mouseout', function() {
-        console.log('mouseout, punch b&w');
+    punch.addEventListener("mouseout", function() {
+        console.log("mouseout, punch b&w");
         punch.src = "img/bw/punch-bw.png";
     });
 
-    punch.addEventListener('click', function() {
-        console.log('punch clicked');
-        punchAnswer.style.display = 'block';
-        document.getElementById("punchAnswer").className = "answer";
+    punch.addEventListener("click", function() {
+        console.log("punch clicked");
+        punchAnswer.style.display = "block";
+        //document.getElementById("punchAnswer").className = "answer";
     });
 
 // -----------------------------------------------------
     // change bad color
-    bad.addEventListener('mouseover', function() {
-        console.log('mouseover, change color');
-        bad.src = "img/bw/bad.png"
+    bad.addEventListener("mouseover", function() {
+        console.log("mouseover, change color");
+        bad.src = "img/bw/bad.png";
     });
 
-    bad.addEventListener('mouseout', function() {
-        console.log('mouseout, bad b&w');
+    bad.addEventListener("mouseout", function() {
+        console.log("mouseout, bad b&w");
         bad.src = "img/bw/bad-bw.png";
     });
 
-    bad.addEventListener('click', function() {
-        console.log('punch clicked');
-        badAnswer.style.display = 'block';
-        document.getElementById("badAnswer").className = "answer";
+    bad.addEventListener("click", function() {
+        console.log("bad clicked");
+        badAnswer.style.display = "block";
+      //  document.getElementById("badAnswer").className = "answer";
     });
 
 // -----------------------------------------------------
     // change marvels color
-    marvels.addEventListener('mouseover', function() {
-        console.log('mouseover, change color');
-        marvels.src = "img/bw/marvels.png"
+    marvels.addEventListener("mouseover", function() {
+        console.log("mouseover, change color");
+        marvels.src = "img/bw/marvels.png";
     });
 
-    marvels.addEventListener('mouseout', function() {
-        console.log('mouseout, marvels b&w');
+    marvels.addEventListener("mouseout", function() {
+        console.log("mouseout, marvels b&w");
         marvels.src = "img/bw/marvel-bw.png";
     });
 
-    marvels.addEventListener('click', function() {
-        console.log('punch clicked');
-        marvelAnswer.style.display = 'block';
-        document.getElementById("marvelAnswer").className = "answer";
+    marvels.addEventListener("click", function() {
+        console.log("marvels clicked");
+        marvelAnswer.style.display = "block";
+      //  document.getElementById("marvelAnswer").className = "answer";
     });
 
 // -----------------------------------------------------
     // change gb color
-    gb.addEventListener('mouseover', function() {
-        console.log('mouseover, change color');
-        gb.src = "img/bw/gb.png"
+    gb.addEventListener("mouseover", function() {
+        console.log("mouseover, change color");
+        gb.src = "img/bw/gb.png";
     });
 
-    gb.addEventListener('mouseout', function() {
-        console.log('mouseout, gb b&w');
+    gb.addEventListener("mouseout", function() {
+        console.log("mouseout, gb b&w");
         gb.src = "img/bw/gb-bw.png";
     });
 
-    gb.addEventListener('click', function() {
-        console.log('punch clicked');
-        gbAnswer.style.display = 'block';
-        document.getElementById("gbAnswer").className = "answer";
+    gb.addEventListener("click", function() {
+        console.log("gb clicked");
+        gbAnswer.style.display = "block";
+      //  document.getElementById("gbAnswer").className = "answer";
     });
 });
 // ----------------------------------------------
